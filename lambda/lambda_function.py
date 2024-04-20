0c012e13900d10c3c3fe5fae0c7c235a3f4ce3df
Julio Henrique <73672593+JulioH-dot@users.noreply.github.com>
2024-04-19 22:24:08 -0300

    Update lambda_function.py :zap:

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,10 @@
 import json
-from produtos import get_product_by_id, get_all_products
+
+products = [
+    {'id': 1, 'name': 'Product 1', 'price': 10.0},
+    {'id': 2, 'name': 'Product 2', 'price': 20.0},
+    {'id': 3, 'name': 'Product 3', 'price': 30.0}
+]
 
 def lambda_handler(event, context):
     
@@ -33,3 +38,12 @@ def gerar_resposta(status_code: int, mensagem: str = None, response: dict = None
         'headers': headers,
         'body': json.dumps(body, default=str, ensure_ascii=False)
     }
+
+def get_product_by_id(product_id):
+    product = next((p for p in products if p['id'] == product_id), None)
+    if not product:
+        raise Exception('Product not found')
+    return product
+
+def get_all_products():
+    return products
