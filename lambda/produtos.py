12c455f95a1a2f57fa200d80cf22114969fb317d
Julio Henrique <73672593+JulioH-dot@users.noreply.github.com>
2024-04-19 22:06:42 -0300

    Create archive produtos.py :rocket:

---
+++ lambda/produtos.py
@@ -0,0 +1,16 @@
+import json
+
+products = [
+    {'id': 1, 'name': 'Product 1', 'price': 10.0},
+    {'id': 2, 'name': 'Product 2', 'price': 20.0},
+    {'id': 3, 'name': 'Product 3', 'price': 30.0}
+]
+
+def get_product(product_id):
+    product = next((p for p in products if p['id'] == product_id), None)
+    if not product:
+        raise Exception('Product not found')
+    return product
+
+def get_all_products():
+    return products
