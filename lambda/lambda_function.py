ceb93df7c1d16eb3942b22efa21bae3c4d13ee11
Julio Henrique <73672593+JulioH-dot@users.noreply.github.com>
2024-04-19 22:09:02 -0300

    Update lambda_function.py to recive imports from produtos.py :rocket:

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,5 @@
 import json
-
-products = [
-    {'id': 1, 'name': 'Product 1', 'price': 10.0},
-    {'id': 2, 'name': 'Product 2', 'price': 20.0},
-    {'id': 3, 'name': 'Product 3', 'price': 30.0}
-]
+from produtos import get_product, get_all_products
 
 def lambda_handler(event, context):
     
@@ -38,13 +33,3 @@ def gerar_resposta(status_code: int, mensagem: str = None, response: dict = None
         'headers': headers,
         'body': json.dumps(body, default=str, ensure_ascii=False)
     }
-
-
-def get_product_by_id(product_id):
-    product = next((p for p in products if p['id'] == product_id), None)
-    if not product:
-        raise Exception('Product not found')
-    return product
-
-def get_all_products():
-    return products
